73021c9b28dcd60054092c2df7c34f557f516493
Matthew Creek <matthew.creek27@gmail.com>
2023-04-21 23:26:58 -0500

    capitalization bug fixed in css

--- src/data.js
+++ src/data.js
@@ -8,14 +8,14 @@ export default [
     },
     {
         id:2,
-        country: "JAPAN",
+        country: "Japan",
         locationName: "Akihabara",
         date: "September 3rd - September 23rd, 2025",
         description: "Akihabara is considered by many to be the epicentre of modern Japanese otaku culture, and is a major shopping district for video games, anime, manga, electronics and computer-related goods. Icons from popular anime and manga are displayed prominently on the shops in the area, and numerous maid cafés and some arcades are found throughout the district.",
     },
     {
         id:3,
-        country: "ENGLAND",
+        country: "England",
         locationName: "Big Ben",
         date: "March 23rd, 2008",
         description: "Big Ben is the nickname for the Great Bell of the Great Clock of Westminster, at the north end of the Palace of Westminster in London, England, and the name is frequently extended to refer also to the clock and the clock tower. The official name of the tower in which Big Ben is located was originally the Clock Tower, but it was renamed Elizabeth Tower in 2012 to mark the Diamond Jubilee of Elizabeth II.",
